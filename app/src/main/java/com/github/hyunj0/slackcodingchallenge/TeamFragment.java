4421f67c7b726e6016749b82ef44cad6586d60b7
Sarah H. Kim <hyunj0@hotmail.com>
2016-02-05 05:38:59 -0500

    data persistence for offline use

--- app/src/main/java/com/github/hyunj0/slackcodingchallenge/TeamFragment.java
+++ app/src/main/java/com/github/hyunj0/slackcodingchallenge/TeamFragment.java
@@ -1,5 +1,9 @@
 package com.github.hyunj0.slackcodingchallenge;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.github.hyunj0.slackcodingchallenge.api.SlackClient;
 import com.github.hyunj0.slackcodingchallenge.model.Member;
@@ -22,7 +27,17 @@ import retrofit2.Response;
 
 public class TeamFragment extends Fragment {
 
+    public static final String PREFERENCE_FILE = "com.github.hyunj0.slackPersistence";
+    public static final String JSON = "json";
+
     private static final String TOKEN = "xoxp-5048173296-5048487710-19045732087-b5427e3b46";
+
+    private ConnectivityManager connectivityManager;
+    private NetworkInfo activeNetwork;
+    private boolean isConnected;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private String json;
 
     private RecyclerView rv;
     private List<Member> teamMembers;
@@ -30,6 +45,16 @@ public class TeamFragment extends Fragment {
 
     public TeamFragment() {
 
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        activeNetwork = connectivityManager.getActiveNetworkInfo();
+        isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        sharedPreferences = getActivity().getSharedPreferences(PREFERENCE_FILE, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
     }
 
     @Override
@@ -44,25 +69,42 @@ public class TeamFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        Call<SlackTeam> call = SlackClient.getServiceClient().getSlackTeam(TOKEN);
-        call.enqueue(new Callback<SlackTeam>() {
-            @Override
-            public void onResponse(Call<SlackTeam> call, Response<SlackTeam> response) {
-                SlackTeam slackTeam = response.body();
-                String json = new Gson().toJson(slackTeam);
-                Log.d("SUCCESS", json);
-                teamMembers = slackTeam.getMembers();
-                for (Member teamMember : teamMembers) {
-                    Log.d("TEAM MEMBER FOUND", teamMember.toString());
+        if (isConnected) {
+            Call<SlackTeam> call = SlackClient.getServiceClient().getSlackTeam(TOKEN);
+            call.enqueue(new Callback<SlackTeam>() {
+                @Override
+                public void onResponse(Call<SlackTeam> call, Response<SlackTeam> response) {
+                    SlackTeam slackTeam = response.body();
+                    json = new Gson().toJson(slackTeam);
+                    teamMembers = slackTeam.getMembers();
+                    adapter = new MemberAdapter(getContext(), teamMembers);
+                    rv.setAdapter(adapter);
                 }
+
+                @Override
+                public void onFailure(Call<SlackTeam> call, Throwable t) {
+                    Log.d("FAILURE", t.toString());
+                }
+            });
+        } else {
+            String cache = sharedPreferences.getString(JSON, null);
+            if (savedInstanceState == null && cache == null) {
+                Toast.makeText(getContext(), "Enable network", Toast.LENGTH_LONG).show();
+                getActivity().findViewById(R.id.no_network).setVisibility(View.VISIBLE);
+            } else {
+                SlackTeam slackTeam = new Gson().fromJson(cache, SlackTeam.class);
+                teamMembers = slackTeam.getMembers();
                 adapter = new MemberAdapter(getContext(), teamMembers);
                 rv.setAdapter(adapter);
             }
+        }
+    }
 
-            @Override
-            public void onFailure(Call<SlackTeam> call, Throwable t) {
-                Log.d("FAILURE", t.toString());
-            }
-        });
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (isConnected) {
+            sharedPreferences.edit().putString(JSON, json).commit();
+        }
     }
 }
