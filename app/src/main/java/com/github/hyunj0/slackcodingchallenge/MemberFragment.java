fe72585d59ab32d0d437f1e75cd824112354c32a
Sarah H. Kim <hyunj0@hotmail.com>
2016-02-04 23:26:48 -0500

    initial commit

---
+++ app/src/main/java/com/github/hyunj0/slackcodingchallenge/MemberFragment.java
@@ -0,0 +1,20 @@
+package com.github.hyunj0.slackcodingchallenge;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MemberFragment extends Fragment {
+
+    public MemberFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_member, container, false);
+        return v;
+    }
+}
